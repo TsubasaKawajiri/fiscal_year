89156de4c8072755f21d778802cc2f38f2a6a1d9
Tsubasa Kawajiri <noodles@aznyn.jp>
2023-03-15 20:23:52 +0900

    add shared_context

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,3 +13,44 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 end
+
+RSpec.shared_context "fiscal_year_start_jan" do
+  before do
+    FiscalYear.configure do |config|
+      config.start_month = 1
+    end
+  end
+end
+
+RSpec.shared_context "fiscal_year_start_apr" do
+  before do
+    FiscalYear.configure do |config|
+      config.start_month = 4
+    end
+  end
+end
+
+RSpec.shared_context "fiscal_year_start_june" do
+  before do
+    FiscalYear.configure do |config|
+      config.start_month = 6
+    end
+  end
+end
+
+RSpec.shared_context "fiscal_year_start_aug" do
+  before do
+    FiscalYear.configure do |config|
+      config.start_month = 8
+    end
+  end
+end
+
+RSpec.shared_context "fiscal_year_start_dec" do
+  before do
+    FiscalYear.configure do |config|
+      config.start_month = 12
+    end
+  end
+end
+
