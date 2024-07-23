0949c19be38e4aebaf708eb0d12cfdf5f11824cd
Tsubasa Kawajiri <tk.music.jpn@gmail.com>
2024-07-24 04:27:36 +0900

    improve configure start month

--- lib/fiscal_year/config.rb
+++ lib/fiscal_year/config.rb
@@ -1,11 +1,54 @@
 # frozen_string_literal: true
 
+require "fiscal_year/invalid_start_month_error"
+
 module FiscalYear
   class Config
-    attr_accessor :start_month
+    attr_reader :start_month
+
+    VALID_START_MONTHS = {
+      jan: 1,
+      feb: 2,
+      mar: 3,
+      apr: 4,
+      may: 5,
+      jun: 6,
+      jul: 7,
+      aug: 8,
+      sep: 9,
+      oct: 10,
+      nov: 11,
+      dec: 12
+    }.freeze
 
     def initialize
       @start_month = 4
+    end
+
+    def set_start_month(month)
+      valid_start_month?(month)
+
+      @start_month = fetch_start_month(month)
+
+      true
+    end
+
+    private
+
+    def valid_start_month?(month)
+      if month.is_a? Integer
+        VALID_START_MONTHS.values.include?(month)
+      else
+        VALID_START_MONTHS.keys.include?(month)
+      end || raise(InvalidStartMonthError)
+    end
+
+    def fetch_start_month(month)
+      if month.is_a? Integer
+        VALID_START_MONTHS.rassoc(month)&.last
+      else
+        VALID_START_MONTHS.assoc(month)&.last
+      end || raise(StandardError)
     end
   end
 end
---
+++ spec/fiscal_year/config_spec.rb
@@ -0,0 +1,93 @@
+# frozen_string_literal: true
+
+RSpec.describe FiscalYear::Config do
+  describe "#initialize" do
+    it "初期値は4が設定されている" do
+      config = FiscalYear::Config.new
+
+      expect(config.start_month).to eq(4)
+    end
+  end
+
+  describe "#set_start_month" do
+    context "引数が整数の場合" do
+      it "start_monthに数値で設定される" do
+        config = FiscalYear::Config.new
+
+        config.set_start_month(1)
+
+        expect(config.start_month).to eq(1)
+      end
+    end
+
+    context "引数がシンボルの場合" do
+      it "start_monthに数値で設定される" do
+        config = FiscalYear::Config.new
+
+        config.set_start_month(:jan)
+
+        expect(config.start_month).to eq(1)
+      end
+    end
+
+    context "引数が不正な場合" do
+      it "InvalidStartMonthErrorが発生する" do
+        config = FiscalYear::Config.new
+
+        expect { config.set_start_month(13) }.to raise_error(FiscalYear::InvalidStartMonthError)
+      end
+    end
+  end
+
+  describe "#valid_start_month?" do
+    context "引数が整数の場合" do
+      it "trueが返る" do
+        config = FiscalYear::Config.new
+
+        expect(config.send(:valid_start_month?, 1)).to be_truthy
+      end
+    end
+
+    context "引数がシンボルの場合" do
+      it "trueが返る" do
+        config = FiscalYear::Config.new
+
+        expect(config.send(:valid_start_month?, :jan)).to be_truthy
+      end
+    end
+
+    context "引数が不正な場合" do
+      it "InvalidStartMonthErrorが発生する" do
+        config = FiscalYear::Config.new
+
+        expect { config.send(:valid_start_month?, 13) }.to raise_error(FiscalYear::InvalidStartMonthError)
+      end
+    end
+  end
+
+  describe "#fetch_start_month" do
+    context "引数が整数の場合" do
+      it "数値が返る" do
+        config = FiscalYear::Config.new
+
+        expect(config.send(:fetch_start_month, 1)).to eq(1)
+      end
+    end
+
+    context "引数がシンボルの場合" do
+      it "数値が返る" do
+        config = FiscalYear::Config.new
+
+        expect(config.send(:fetch_start_month, :jan)).to eq(1)
+      end
+    end
+
+    context "引数が不正な場合" do
+      it "StandardErrorが発生する" do
+        config = FiscalYear::Config.new
+
+        expect { config.send(:fetch_start_month, 13) }.to raise_error(StandardError)
+      end
+    end
+  end
+end
